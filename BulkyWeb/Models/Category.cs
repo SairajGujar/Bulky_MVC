cb86cd878f1f45471b7a22ef09fd45c0a4435513
SAiii0101 <sairajgujar2000@gmail.com>
2025-04-17 12:29:56 +0530

    Create Category operation added, Lux theme added.

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace BulkyWeb.Models
@@ -8,7 +9,11 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30, ErrorMessage = "Name cannot be longer than 30 characters")]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100")]
         public int DisplayOrder { get; set; }
     }
 }
